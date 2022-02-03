ae515ae112ac66d73e4fff996d515a31ed1afa5f
KenethSandoval <kenethra.74@gmail.com>
2022-02-02 22:58:51 -0600

    feat: uploadCSV

--- src/internal/router/routes.go
+++ src/internal/router/routes.go
@@ -3,21 +3,26 @@ package router
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/KenethSandoval/xopopu/internal/upload"
 )
 
 type Handler struct{}
 
-func (Handler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+type MessageStruct struct {
+	Message string `json:"message"`
+}
 
+func (Handler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func InitRouter(mux *http.ServeMux) {
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/upload", upload.UploadCSV)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	type MessageStruct struct {
-		Message string `json:"message"`
-	}
+func home(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(MessageStruct{Message: "hello"})
 }
---
+++ src/internal/upload/upload.ctrl.go
@@ -0,0 +1,58 @@
+package upload
+
+import (
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+)
+
+// UploadCSV Receive the formdata file and make sure it is a valid .csv
+func UploadCSV(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	defer file.Close()
+
+	mimeType := handler.Header.Get("Content-Type")
+
+	switch mimeType {
+	case "image/png":
+		// validar error
+		fmt.Println("Solo se permite csv")
+	default:
+		// subir archivo
+		saveFile(w, file, handler)
+	}
+}
+
+func saveFile(w http.ResponseWriter, file multipart.File, handler *multipart.FileHeader) {
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	fmt.Println(handler.Filename)
+
+	err = ioutil.WriteFile("./files/"+handler.Filename, data, 0666)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	jsonResponse(w, 200, "File uploaded successfully!")
+}
+
+func jsonResponse(w http.ResponseWriter, code int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	fmt.Fprint(w, message)
+}
